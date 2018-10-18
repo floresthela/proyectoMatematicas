f8c979a0c4fd5d76cac5018f83aad0c3a6624315
flor <esthelabarbosa@gmail.com>
2018-10-18 18:20:43 -0500

    hola

---
+++ proyectoMatematicas/ListaSeccionesConicas.swift
@@ -0,0 +1,19 @@
+//
+//  ListaSeccionesConicas.swift
+//  proyectoMatematicas
+//
+//  Created by Flor Barbosa on 10/18/18.
+//  Copyright © 2018 Flor Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class ListaSeccionesConicas: NSObject {
+    
+    var nombre : String!
+    
+    init(nombre : String) {
+        self.nombre = nombre
+    }
+
+}
