8df6b706a1ace0abf763dd7b1ad5ed4c4fcda5da
flor <esthelabarbosa@gmail.com>
2018-10-20 19:57:56 -0500

    se agregaron .swift y botón de regresar funcionan

---
+++ proyectoMatematicas/PracticaViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PracticaViewController.swift
+//  proyectoMatematicas
+//
+//  Created by Flor Barbosa on 10/20/18.
+//  Copyright © 2018 Flor Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class PracticaViewController: UIViewController {
+
+    
+    @IBAction func regresaMenu(_ sender: UIBarButtonItem) {
+          dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ proyectoMatematicas/QuizViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuizViewController.swift
+//  proyectoMatematicas
+//
+//  Created by Flor Barbosa on 10/20/18.
+//  Copyright © 2018 Flor Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func regresaMenu(_ sender: Any) {
+          dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
