85a8325285d5759dd0edec9ba6c8a8468f6dc55b
flor <esthelabarbosa@gmail.com>
2018-10-29 14:54:53 -0600

    practica

--- proyectoMatematicas/graficaCirculoViewController.swift
+++ proyectoMatematicas/graficaCirculoViewController.swift
@@ -12,65 +12,71 @@ import Charts
 class graficaCirculoViewController: UIViewController, ChartViewDelegate {
 
     
-    @IBOutlet weak var tfX: UITextField!
-    @IBOutlet weak var tfY: UITextField!
-    @IBOutlet weak var tfR: UITextField!
+  
     
     @IBOutlet weak var chartView: LineChartView!
     
-    @IBOutlet weak var sv1: UIStackView!
-    @IBOutlet weak var sv0: UIStackView!
-    
+
     var vX : Double!
     var vY : Double!
     var vR : Double!
-    var points = [Double]()
-    
-    @IBOutlet weak var segCon: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         chartView.delegate = self
-        sv1.isHidden = true
 
         vX = 1
         vY = 1
+        vR = 100
+    
         
-        if segCon.selectedSegmentIndex == 0{
-            vX = 1
-            vY = 1
-        }
-        else{
-            //vX = Double(tfX.text!)
-            //vY = Double(tfY.text!)
-        }
-        
-        chartView.backgroundColor = .white
         chartView.chartDescription?.enabled = false
+        
         chartView.dragEnabled = true
         chartView.setScaleEnabled(true)
         chartView.pinchZoomEnabled = true
+        chartView.highlightPerDragEnabled = true
+        
+        chartView.backgroundColor = .white
+    
+        chartView.legend.enabled = false
 
-        // x-axis limit line
-        let llXAxis = ChartLimitLine(limit: 5, label: "Index 10")
-        llXAxis.lineWidth = 10
-        llXAxis.lineDashLengths = [10, 10, 0]
-        llXAxis.labelPosition = .rightBottom
-        llXAxis.valueFont = .systemFont(ofSize: 10)
         
-        chartView.xAxis.gridLineDashLengths = [1, 1]
-        //chartView.xAxis.gridLineDashPhase = 0
+        let xAxis = chartView.xAxis
+        //xAxis.labelPosition = .bottom
+        xAxis.labelFont = .systemFont(ofSize: 10, weight: .light)
+        xAxis.labelTextColor = UIColor.black
+        xAxis.drawAxisLineEnabled = true
+        xAxis.drawGridLinesEnabled = true
+        xAxis.gridLineWidth = 1
+        xAxis.gridLineDashLengths = [1,1]
+        xAxis.axisMaximum = 200
+        xAxis.axisMinimum = -200
         
-        chartView.leftAxis.axisMinimum = -20
-        chartView.leftAxis.axisMaximum = 20
-        chartView.rightAxis.axisMinimum = -20
-        chartView.rightAxis.axisMaximum = 20
-        chartView.xAxis.axisMinimum = -20
-        chartView.xAxis.axisMaximum = 20
+        xAxis.granularity = 0.5
+        let leftAxis = chartView.leftAxis
+        leftAxis.labelPosition = .insideChart
+        leftAxis.labelFont = .systemFont(ofSize: 12, weight: .light)
+        leftAxis.drawGridLinesEnabled = true
+        leftAxis.gridLineWidth = 1
+        leftAxis.gridLineDashLengths = [1,1]
+        leftAxis.granularityEnabled = true
+        leftAxis.axisMinimum = -200
+        leftAxis.axisMaximum = 200
+        leftAxis.granularity = 0.5
+        leftAxis.yOffset = -9
+        leftAxis.labelTextColor = UIColor.black
         
-        vR = 100
-        setDataCount(100)
-        chartView.animate(xAxisDuration: 2.5)
+        chartView.xAxis.gridLineDashLengths = [1,1]
+        chartView.rightAxis.enabled = false
+        
+        chartView.legend.form = .line
+        
+    
+        setDataCount()
+        //chartView.animate(xAxisDuration: 2.5)
+        
         // Do any additional setup after loading the view.
     }
     
@@ -82,76 +88,49 @@ class graficaCirculoViewController: UIViewController, ChartViewDelegate {
     }
     
     
-    @IBAction func scOrigen(_ sender: UISegmentedControl) {
-        switch segCon.selectedSegmentIndex
-        {
-        case 0:
-            sv0.isHidden = false
-            sv1.isHidden = true
-            UIView.animate(withDuration: 1, animations:{
-                self.tfR.frame.origin.x = 237
-            })
-            tfR.text = ""
-            vX = 1
-            vY = 1
-        case 1:
-            sv1.isHidden = false
-            sv0.isHidden = true
-            UIView.animate(withDuration: 1, animations:{
-                self.tfR.frame.origin.x = 291
-            })
-            tfR.text = ""
-        default:
-            break
-        }
-    }
     
     @IBAction func bGrafica(_ sender: UIButton){
         // número que se pasa de parámetro es el rango en X
-         vR = Double(tfR.text!)
         vR = 100
-        setDataCount(100)
+        setDataCount()
+        chartView.animate(xAxisDuration: 2.5)
     }
     
     
-    func setDataCount(_ count: Int) {
+    func setDataCount() {
         
-        let yVal1 = (-20..<count).map {
-            return ChartDataEntry(x: Double($0), y: sqrt(vR - pow(Double($0) - vX, 2)))
+        let yVal1 = (-200...200).map {
+            //return ChartDataEntry(x: Double($0), y: sqrt(vR - pow(Double($0) - vX, 2)))
+            //return ChartDataEntry(x: Double($0), y: Double(sqrt(100 - pow(Double($0), 2))))
+            return ChartDataEntry(x: Double($0)/10, y: sqrt(vR - pow(Double($0)/10, 2)))
+            //return ChartDataEntry(x: Double($0)/10, y: pow(Double($0),2))
+           
         }
         
-        let yVal2 = (-20..<count).map {
-            return ChartDataEntry(x: Double($0), y: sqrt(vR - pow(Double($0) - vX, 2)) * -1)
+        let yVal2 = (-50...50).map {
+            //return ChartDataEntry(x: Double($0)/10, y: sqrt((vR - (vX * pow(Double($0)/10, 2))) / vY) * -1)
+           return ChartDataEntry(x: Double($0)/10, y: sqrt(vR - pow(Double($0)/10, 2)))
         }
-        let set1 = LineChartDataSet(values: yVal1, label: "DataSet 1")
-         set1.axisDependency = .left
-         set1.setColor(UIColor(red: 255/255, green: 241/255, blue: 46/255, alpha: 1))
+        
+        let set1 = LineChartDataSet(values: yVal1, label: "Arriba")
+         //set1.axisDependency = .left
+         set1.setColor(UIColor.red)
          set1.drawCirclesEnabled = false
          set1.lineWidth = 2
-         set1.circleRadius = 3
-         set1.fillAlpha = 1
-         set1.drawFilledEnabled = true
-         set1.fillColor = .white
-         set1.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
-         set1.drawCircleHoleEnabled = false
-
-        let set2 = LineChartDataSet(values: yVal2, label: "x")
-        set2.axisDependency = .left
-        set2.setColor(UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1))
-        set1.drawCirclesEnabled = true
+        set1.mode = .cubicBezier
+        
+        let set2 = LineChartDataSet(values: yVal2, label: "Abajo")
+        set2.setColor(UIColor.black)
+        set2.drawCirclesEnabled = false
         set2.lineWidth = 2
-        set2.circleRadius = 3
-        set2.fillAlpha = 1
-        set1.drawFilledEnabled = false
-        
-        set2.fillColor = UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)
-        set2.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
-        set2.drawCircleHoleEnabled = false
-        
+        set2.mode = .cubicBezier
+
         let data = LineChartData(dataSets: [set1,set2])
-        data.setDrawValues(false)
-        data.setValueTextColor(.white)
+        //let data = LineChartData(dataSet: set1)
+        
+        //data.setValueTextColor(.white)
         data.setValueFont(.systemFont(ofSize: 9))
+        data.setDrawValues(false)
         chartView.data = data
     }
     
