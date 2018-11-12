9873af61bf6ea1a5a043cceb1eadfa5f0be97bb8
Alumno <Alumno@laboratorios-macbook-pro-6.local>
2018-11-12 15:52:13 -0600

    Cambie colores del menu principal y le quite el fondo a una imagen

--- proyectoMatematicas/ViewController.swift
+++ proyectoMatematicas/ViewController.swift
@@ -11,26 +11,18 @@ import Charts
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var boton1: UIButton!
     @IBOutlet weak var boton2: UIButton!
     @IBOutlet weak var boton3: UIButton!
     @IBOutlet weak var boton4: UIButton!
-    
-    
-    //var colorFondo = UIColor(red: 17/255.0, green: 93/255.0, blue: 171/255.0, alpha: 1)
-    
     @IBOutlet var viewMenu: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       // self.viewMenu.backgroundColor = colorFondo
-        boton1.backgroundColor = UIColor(red: 149/255.0, green: 198/255.0, blue: 35/255.0, alpha: 1)
-        boton2.backgroundColor = UIColor(red: 149/255.0, green: 198/255.0, blue: 35/255.0, alpha: 1)
-        boton3.backgroundColor = UIColor(red: 149/255.0, green: 198/255.0, blue: 35/255.0, alpha: 1)
-        boton4.backgroundColor = UIColor(red: 149/255.0, green: 198/255.0, blue: 35/255.0, alpha: 1)
+        boton1.backgroundColor = UIColor(red: 0/255.0, green: 173/255.0, blue: 65/255.0, alpha: 1)
+        boton2.backgroundColor = UIColor(red: 0/255.0, green: 173/255.0, blue: 65/255.0, alpha: 1)
+        boton3.backgroundColor = UIColor(red: 0/255.0, green: 173/255.0, blue: 65/255.0, alpha: 1)
+        boton4.backgroundColor = UIColor(red: 0/255.0, green: 173/255.0, blue: 65/255.0, alpha: 1)
 }
-
-
-
 }
 
