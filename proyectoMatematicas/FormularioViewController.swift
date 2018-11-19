411c117b349f0c00c78228924c7e24f573d7ba9d
sofiacetre <sct2896@gmail.com>
2018-11-19 16:06:12 -0600

    Arreglamos sección de formulario, quite zoom de formulario, imágenes mas bonitas.

--- proyectoMatematicas/FormularioViewController.swift
+++ proyectoMatematicas/FormularioViewController.swift
@@ -37,9 +37,10 @@ class FormularioViewController: UIViewController, UIScrollViewDelegate {
         
         
         scrollView.delegate = self
-        scrollView.minimumZoomScale = 1.0
-        scrollView.maximumZoomScale = 3.0
-        scrollView.zoomScale = 1.0
+       // scrollView.minimumZoomScale = 1.0
+       // scrollView.maximumZoomScale = 3.0
+       // scrollView.zoomScale = 1.0
+        
 
         //let imageViewSize = img4.bounds.size
         //let scrollViewSize = scrollView.bounds.size
@@ -55,16 +56,19 @@ class FormularioViewController: UIViewController, UIScrollViewDelegate {
         //img4.addGestureRecognizer(pinchGesture)
         
         //lbTitulo.text = labelText
+        
+        
         scrollView.contentSize = viewFormulario.frame.size
+        
         
         if seccConic == "Círculo" {
             navTitulo.title = "Círculo"
             label1.text = "Un círculo es todos los puntos en un plano que están a una distancia fija de un punto dado en el plano. El punto dado se llama centro, (h, k), y la distancia fija se llama radio, r, del círculo."
             img4.image = UIImage(named: "descCirculo")
             label2.text = "La forma estándar de la ecuación de un círculo con centro en (0,0) tiene la siguiente forma: "
-            img1.image = UIImage(named: "circuloEcuacion")
+            img1.image = UIImage(named: "ecuacionCirculoFINAL")
             label3.text = "La forma estándar de la ecuación de un círculo con centro en (h,k) es:"
-            img2.image = UIImage(named: "formaCirculo2")
+            img2.image = UIImage(named: "ecuacionCirculo2FINAL")
 
         }
         if seccConic == "Hiperbola"{
@@ -73,35 +77,37 @@ class FormularioViewController: UIViewController, UIScrollViewDelegate {
             label1.text = "Una hipérbola es todos los puntos en un plano donde la diferencia de sus distancias desde dos puntos fijos es constante. Cada uno de los puntos fijos se llama foco de la hipérbola."
             img4.image = UIImage(named: "descHiperbola")
             label2.text = "La forma estándar de la ecuación de una hipérbola con centro en (0,0) puede tener cualquiera de estas dos formas: "
-            img1.image = UIImage(named: "formulaHiperbola")
+            img1.image = UIImage(named: "ecuacionHiperbolaFINAL")
             label3.text = "La forma estándar de la ecuación de una hipérbola con centro en (h,k) puede tener cualquiera de las siguientes formas: "
-            img2.image = UIImage(named: "hiperbola2")
+            img2.image = UIImage(named: "ecuacionHiperbola2FINAL")
         }
         if seccConic == "Parábola"{
             navTitulo.title = "Parábola"
             label1.text = "Una parábola es todos los puntos en un plano que están a la misma distancia de un punto fijo y de una línea fija. El punto fijo se llama el foco, y la línea fija se llama la directriz de la parábola. "
             img4.image = UIImage(named: "descParabola")
             label2.text = "La ecuación general de la parabola tiene la siguiente forma: "
-            img1.image = UIImage(named: "formaGeneralParabola")
+            img1.image = UIImage(named: "ecuacionParabolaFINAL")
             label3.text = "La forma estándar de la parabola tiene la siguiente forma: "
-            img2.image = UIImage(named: "formaEstandarParabola")
+            img2.image = UIImage(named: "ecuacionParabola2FINAL")
         }
         if seccConic == "Elipse"{
             navTitulo.title = "Elipse"
             label1.text = "Un elipse es todos los puntos de un plano donde la suma de las distancias desde dos puntos fijos es constante. Cada uno de los puntos fijos se llama foco del  elipse. "
             img4.image = UIImage(named: "descElipse")
             label2.text = "La forma estándar de la ecuación de un elipse con centro en (0,0) tiene la siguiente forma:"
-            img1.image = UIImage(named: "elipseFormula")
+            img1.image = UIImage(named: "ecuacionElipseFINAL")
             label3.text = "La forma estándar de la ecuación de un elipse con centro de (h,k) tiene la siguiente forma:"
-            img2.image = UIImage(named: "ElipseCentro2")
+            img2.image = UIImage(named: "ecuacionElipse2FINAL")
         }
         
     }
     
+    /****
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         
         return img4
     }
+    *///
     // tap a uiimage
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         let imageView = sender.view as! UIImageView
